edfc3141ca6edaa25cfc40e9feb8f2e60f08d542
asharan2511 <sharan.subham@gmail.com>
2025-11-20 19:35:30 +0530

    initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import dotenv from "dotenv";
+const app = express();
+app.use(express.json());
+dotenv.config();
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
